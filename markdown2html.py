97bb35434fdb90136cab185f806feedec5149325
JustGodWork <discord1elie@gmail.com>
2024-08-07 20:42:25 +0200

    Added task 1

--- markdown2html.py
+++ markdown2html.py
@@ -5,6 +5,7 @@
 """
 
 from sys import argv, exit, stderr
+from typing import List
 from os import path
 
 
@@ -13,13 +14,54 @@ def open_file(filename: str) -> str:
     if not path.isfile(filename):
         raise FileNotFoundError(f'Missing {filename}')
     with open(filename, 'r') as f:
-        text = f.read()
+        text = f.readlines()
     return text
 
 
-def markdown2html(inputfile: str, outputfile: str = None) -> None:
+def save_file(filename: str, text: str) -> None:
+    """Saves the text to a file."""
+    if (not filename.endswith('.html')):
+        filename += '.html'
+    with open(filename, 'w') as f:
+        f.write(text)
+
+
+def get_heading_level(line: str) -> int:
+    """Returns the heading level of a markdown heading."""
+    level = 0
+    for char in line:
+        if char == '#':
+            level += 1
+        else:
+            break
+    return level
+
+
+def heading(text: str, level: int) -> str:
+    """Converts markdown headings to html headings."""
+    # Strip removes leading and trailing whitespaces
+    return f'<h{level}>{text.strip()}</h{level}>'
+
+
+def convert(lines: List[str]) -> str:
+    """Converts markdown to html."""
+
+    html = []
+
+    for line in lines:
+        if (line.startswith('#')):
+            level = get_heading_level(line)
+            # level + 1 because markdown headings as space after the # symbol
+            html.append(heading(line[level + 1:], level))
+
+    return '\n'.join(html)
+
+
+def markdown2html(inputfile: str, outputfile: str) -> None:
     """Converts markdown files to html files."""
-    text = open_file(inputfile)
+    lines = open_file(inputfile)
+    html_content = convert(lines)  # Convert markdown to html
+    save_file(outputfile, html_content)
 
 
 def main() -> None:
