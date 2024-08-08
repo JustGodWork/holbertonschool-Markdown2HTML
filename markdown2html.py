b204f0951131cdaf0a5ac035cc1be67fab2adcdb
JustGodWork <discord1elie@gmail.com>
2024-08-08 04:16:27 +0200

    Updated original index

--- markdown2html.py
+++ markdown2html.py
@@ -37,6 +37,7 @@ def save_file(filename: str, text: str) -> None:
 
     if (not filename.endswith('.html')):
         filename += '.html'
+
     with open(filename, 'w') as f:
         f.write(text)
 
@@ -60,6 +61,7 @@ def get_heading_level(line: str) -> int:
             level += 1
         else:
             break
+
     return level
 
 
@@ -69,7 +71,7 @@ def heading(line: str) -> str:
     # level + 1 because markdown headings as space after the # symbol
     level = get_heading_level(line)
     # Strip removes leading and trailing whitespaces
-    heading_content = line[level + 1:].strip()
+    heading_content = line[level + 1:]
     debug(f'Converting \'{heading_content}\' to heading level {level}')
     return f'<h{level}>{heading_content}</h{level}>'
 
@@ -81,10 +83,10 @@ def list_item(lines: List[str], index: int,
     html = [f'<{list_type}>']
 
     for i in range(index, len(lines)):
-        line = lines[i]
+        line = lines[i].strip()
 
         if (line.startswith(prefix)):
-            li_content = line[2:].strip()
+            li_content = line[2:]
             debug(f'Converting \'{li_content}\' to \'{list_type}\' list item')
             html.append(f'<li>{li_content}</li>')
             index += 1
@@ -117,7 +119,11 @@ def paragraph(lines: List[str], index: int) -> Tuple[
     html_body = '\n<br/>\n'.join(html)
     html_text = f"<p>\n{html_body}\n</p>"
 
-    return index, html_text if (index != original_index) else None
+    if (index != original_index):
+        debug(f'Converted paragraph at index {original_index}, incrementing index to {index}')
+        return index, html_text
+
+    return original_index + 1, None
 
 
 def convert(lines: List[str]) -> str:
@@ -130,12 +136,7 @@ def convert(lines: List[str]) -> str:
 
     # We use a while loop instead of a for loop because we need to skip lines
     while (index < len(lines)):
-        line = lines[index]  # Get the current line as a variable
-
-        if (line == '\n'):
-            debug(f'line {index} is a newline')
-            index += 1  # Go to the next line
-            continue
+        line = lines[index].strip()  # Get the current line as a variable
 
         if (line.startswith('#')):
             debug(f'{line} is a heading')
@@ -168,7 +169,7 @@ def markdown2html(inputfile: str, outputfile: str) -> None:
 
     lines = open_file(inputfile)
     html_content = convert(lines)  # Convert markdown to html
-    save_file(outputfile, html_content)
+    save_file(outputfile, html_content) # Save the html content to a html file
 
 
 def main() -> None:
